504fd88314f6e7037b30a892291c20791c29bcf9
Ashton Sharples <ashsha27@kingshigh.school.nz>
2026-05-15 11:47:38 +1200

    Painting + Lamp (lighting.)

---
+++ omni_light.gd
@@ -0,0 +1,7 @@
+extends OmniLight3D
+
+@export var flicker_strength: float = 0.05
+@export var flicker_speed: float = 20.0
+
+func _process(delta: float) -> void:
+	light_energy = 1.5 + sin(Time.get_ticks_msec() * 0.001 * flicker_speed) * flicker_strength
